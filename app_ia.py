da83e31e95a1392b8fcb054a5db071375d45e3b3
José Carlos <70998210@certus.edu.pe>
2023-10-10 20:12:25 -0500

    Actualizar => Todos los datos

--- app_ia.py
+++ app_ia.py
@@ -1,124 +1,128 @@
 import mysql.connector
-from tkinter import Tk, Label, Button, StringVar, Entry, messagebox, simpledialog
+from mysql.connector import Error
+import getpass  # Para ocultar la contraseña al ingresarla
 
 class CrudApp:
-    def __init__(self, root):
-        # Configuración de la ventana principal
-        self.root = root
-        self.root.title("IA-App")
-
+    def __init__(self):
         # Conexión a la base de datos MySQL
-        self.connection = mysql.connector.connect(
-            host='localhost',
-            database='app_ia',
-            user='root',
-            password=''
-        )
+        self.connection = self.connect_to_database()
 
-        # Crear los elementos de la interfaz gráfica
-        self.create_widgets()
+    def connect_to_database(self):
+        try:
+            connection = mysql.connector.connect(
+                host='localhost',
+                database='app_ia',
+                user='root',
+                password=''
+            )
+            if connection.is_connected():
+                print("Conectado a la base de datos")
+                return connection
+        except Error as e:
+            print(f"Error: {e}")
+            return None
 
-    def create_widgets(self):
-        # Etiqueta principal
-        self.label = Label(self.root, text="LOGIN - CRUD", font=("Tahoma", 16))
-        self.label.grid(row=0, column=0, columnspan=2, pady=10)
-
-        # Variables para almacenar la entrada del usuario
-        self.nombres_var = StringVar()
-        self.apellidos_var = StringVar()
-        self.correo_var = StringVar()
-        self.contrasena_var = StringVar()
-
-        # Entradas y etiquetas para Nombres, Apellidos, Correo y Contraseña
-        Label(self.root, text="Nombres:").grid(row=1, column=0, sticky="e", padx=10, pady=5)
-        Entry(self.root, textvariable=self.nombres_var).grid(row=1, column=1, padx=10, pady=5)
-
-        Label(self.root, text="Apellidos:").grid(row=2, column=0, sticky="e", padx=10, pady=5)
-        Entry(self.root, textvariable=self.apellidos_var).grid(row=2, column=1, padx=10, pady=5)
-        
-        Label(self.root, text="Correo:").grid(row=3, column=0, sticky="e", padx=10, pady=5)
-        Entry(self.root, textvariable=self.correo_var).grid(row=3, column=1, padx=10, pady=5)
-
-        Label(self.root, text="Contraseña:").grid(row=4, column=0, sticky="e", padx=10, pady=5)
-        Entry(self.root, textvariable=self.contrasena_var, show="*").grid(row=4, column=1, padx=10, pady=5)
-
-        # Botones para las operaciones CRUD
-        Button(self.root, text="Insertar", command=self.insertar).grid(row=5, column=0, columnspan=2, pady=10)
-        Button(self.root, text="Seleccionar", command=self.seleccionar).grid(row=6, column=0, columnspan=2, pady=10)
-        Button(self.root, text="Actualizar", command=self.actualizar).grid(row=7, column=0, columnspan=2, pady=10)
-        Button(self.root, text="Eliminar", command=self.eliminar).grid(row=8, column=0, columnspan=2, pady=10)
-
-    def validar_letras(self, input_text):
-        # Función para validar que una cadena contenga solo letras
-        return input_text.isalpha()
+    def close_connection(self):
+        if self.connection.is_connected():
+            self.connection.close()
+            print("Conexión cerrada")
 
     # CRUD => Create Read Update Delete
 
     def insertar(self):
         # Función para insertar un nuevo registro en la base de datos
-        nombres = self.nombres_var.get()
-        apellidos = self.apellidos_var.get()
-        correo = self.correo_var.get()
-        contrasena = self.contrasena_var.get()
+        nombres = input("Nombres: ")
+        apellidos = input("Apellidos: ")
+        correo = input("Correo: ")
+        contrasena = getpass.getpass("Contraseña: ")  # Para ocultar la contraseña
 
-        # Validar que Nombres y Apellidos contengan solo letras
-        if self.validar_letras(nombres) and self.validar_letras(apellidos):
+        try:
             cursor = self.connection.cursor()
             sql_insert = "INSERT INTO usuarios (nombres, apellidos, correo, contraseña) VALUES (%s, %s, %s, %s)"
             cursor.execute(sql_insert, (nombres, apellidos, correo, contrasena))
             self.connection.commit()
-            messagebox.showinfo("Éxito", "Registro insertado!")
-        else:
-            messagebox.showwarning("Advertencia", "Los campos de Nombres y Apellidos deben contener solo letras.")
+            print("Registro insertado!")
+        except Error as e:
+            print(f"Error al insertar: {e}")
 
     def seleccionar(self):
         # Función para seleccionar y mostrar todos los registros de la base de datos
-        cursor = self.connection.cursor()
-        sql_select = "SELECT * FROM usuarios"
-        cursor.execute(sql_select)
-        records = cursor.fetchall()
+        try:
+            cursor = self.connection.cursor()
+            sql_select = "SELECT * FROM usuarios"
+            cursor.execute(sql_select)
+            records = cursor.fetchall()
 
-        result = ""
-        for row in records:
-            result += f"ID = {row[0]}, Nombres = {row[1]}, Apellidos = {row[2]}, Correo = {row[3]}, Contraseña = {row[4]}\n\n"
-
-        messagebox.showinfo("Usuarios", result)
+            for row in records:
+                print(f"ID = {row[0]}, Nombres = {row[1]}, Apellidos = {row[2]}, Correo = {row[3]}, Contraseña = {row[4]}\n")
+        except Error as e:
+            print(f"Error al seleccionar: {e}")
 
     def actualizar(self):
         # Función para actualizar un registro existente en la base de datos
-        idusuario = simpledialog.askinteger("Actualizar Usuario", "Ingrese el ID del usuario a actualizar:", parent=self.root)
-        if idusuario is not None:
-            nuevos_nombres = simpledialog.askstring("Actualizar Usuario", "Nuevos Nombres:", parent=self.root)
-            nuevos_apellidos = simpledialog.askstring("Actualizar Usuario", "Nuevos Apellidos:", parent=self.root)
-            if self.validar_letras(nuevos_nombres) and self.validar_letras(nuevos_apellidos):
-                cursor = self.connection.cursor()
-                sql_update = "UPDATE usuarios SET nombres = %s, apellidos = %s WHERE idusuarios = %s"
-                cursor.execute(sql_update, (nuevos_nombres, nuevos_apellidos, idusuario))
+        idusuario = input("Ingrese el ID del usuario a actualizar: ")
+        try:
+            cursor = self.connection.cursor()
+            cursor.execute(f"SELECT * FROM usuarios WHERE idusuarios = {idusuario}")
+            user_data = cursor.fetchone()
+
+            if user_data:
+                nuevos_nombres = input("Nuevos Nombres: ")
+                nuevos_apellidos = input("Nuevos Apellidos: ")
+                nuevo_correo = input("Nuevo Correo: ")
+                nueva_contrasena = input("Nueva Contraseña: ")
+
+                sql_update = "UPDATE usuarios SET nombres = %s, apellidos = %s, correo = %s, contraseña = %s  WHERE idusuarios = %s"
+                cursor.execute(sql_update, (nuevos_nombres, nuevos_apellidos, nuevo_correo, nueva_contrasena, idusuario))
                 self.connection.commit()
-                messagebox.showinfo("Éxito", "Registro actualizado!")
+                print("Registro actualizado!")
             else:
-                messagebox.showwarning("Advertencia", "Los campos de Nombres y Apellidos deben contener solo letras.")
+                print(f"No se encontró ningún usuario con ID {idusuario}")
+        except Error as e:
+            print(f"Error al actualizar: {e}")
 
     def eliminar(self):
         # Función para eliminar un registro de la base de datos
-        id_usuario = simpledialog.askinteger("Eliminar Usuario", "Ingrese el ID del usuario que desea eliminar:")
-        if id_usuario is not None:
+        id_usuario = input("Ingrese el ID del usuario que desea eliminar: ")
+        try:
             cursor = self.connection.cursor()
-            sql_delete = "DELETE FROM usuarios WHERE idusuarios = %s"
-            cursor.execute(sql_delete, (id_usuario,))
-            self.connection.commit()
-            messagebox.showinfo("Éxito", "Registro eliminado!")
+            cursor.execute(f"SELECT * FROM usuarios WHERE idusuarios = {id_usuario}")
+            user_data = cursor.fetchone()
+
+            if user_data:
+                sql_delete = "DELETE FROM usuarios WHERE idusuarios = %s"
+                cursor.execute(sql_delete, (id_usuario,))
+                self.connection.commit()
+                print("Registro eliminado!")
+            else:
+                print(f"No se encontró ningún usuario con ID {id_usuario}")
+        except Error as e:
+            print(f"Error al eliminar: {e}")
 
 if __name__ == "__main__":
-    root = Tk()
-    app = CrudApp(root)
+    app = CrudApp()
 
-    # Centrar la ventana y establecer su tamaño
-    root.update_idletasks()
-    width = root.winfo_width()
-    height = root.winfo_height()
-    x = (root.winfo_screenwidth() // 2) - (width // 2)
-    y = (root.winfo_screenheight() // 2) - (height // 2)
-    root.geometry(f"{width}x{height}+{x}+{y}")
+    while True:
+        print("\nMenu:")
+        print("1. Insertar")
+        print("2. Seleccionar")
+        print("3. Actualizar")
+        print("4. Eliminar")
+        print("5. Salir")
 
-    root.mainloop()
+        choice = input("Ingrese el número de la opción que desea: ")
+
+        if choice == "1":
+            app.insertar()
+        elif choice == "2":
+            app.seleccionar()
+        elif choice == "3":
+            app.actualizar()
+        elif choice == "4":
+            app.eliminar()
+        elif choice == "5":
+            app.close_connection()
+            print("Saliendo del programa. ¡Hasta luego!")
+            break
+        else:
+            print("Opción no válida. Intente nuevamente.")
