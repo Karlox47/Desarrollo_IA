857bdec46560494bac7c0d826ba47e99cd6dc8f3
José Carlos <70998210@certus.edu.pe>
2023-12-03 23:20:41 -0500

    icono ojito para la contraseña

--- login_ia.py
+++ login_ia.py
@@ -6,6 +6,21 @@ from crud_ia import CrudApp
 from chatbot_ia import mostrar_ventana_chat
 from ttkthemes import ThemedStyle
 
+class PasswordEntry(ttk.Entry):
+    def __init__(self, *args, **kwargs):
+        ttk.Entry.__init__(self, *args, **kwargs)
+        self.show_password = False
+        self.eye_image = ImageTk.PhotoImage(Image.open("F:/Desarrollo_IA/images/eye.png").resize((20, 20)))
+        self.eye_button = ttk.Button(self, image=self.eye_image, command=self.toggle_password_visibility)
+        self.eye_button.place(relx=1.0, rely=0.5, anchor="e")
+
+    def toggle_password_visibility(self):
+        self.show_password = not self.show_password
+        if self.show_password:
+            self.configure(show="")
+        else:
+            self.configure(show="●")
+
 class LoginApp:
     def __init__(self, root, crud_instance):
         self.root = root
@@ -26,7 +41,7 @@ class LoginApp:
         self.logo_image = tk.PhotoImage(file="F:/Desarrollo_IA/images/bot3.png")
 
         self.logo_label = tk.Label(frame, image=self.logo_image)
-        self.logo_label.grid(row=0, column=0, columnspan=2, pady=5)
+        self.logo_label.grid(row=0, column=0, columnspan=3, pady=5)
 
         tk.Label(frame, text="Correo:", font=('Arial', 10, 'bold')).grid(row=1, column=0, pady=5, padx=5, sticky='e')
         tk.Label(frame, text="Contraseña:", font=('Arial', 10, 'bold')).grid(row=2, column=0, pady=5, padx=5, sticky='e')
@@ -34,16 +49,17 @@ class LoginApp:
         style = ThemedStyle(frame)
         style.configure('TEntry', padding=5, relief="flat", fieldbackground="#d3d3d3")  # Ajusta el color de fondo según tu diseño
 
-        self.correo_entry = ttk.Entry(frame, width=25, style="TEntry")
-        self.contraseña_entry = ttk.Entry(frame, show='●', width=25, style="TEntry")  # Usar '●' en lugar de '*'
+        self.correo_entry = ttk.Entry(frame, width=32, style="TEntry")
+        self.contraseña_entry = PasswordEntry(frame, show='●', width=41, style="TEntry")  # Ajusta el ancho según tus necesidades
         self.correo_entry.grid(row=1, column=1, pady=5, padx=5, sticky='w')
-        self.contraseña_entry.grid(row=2, column=1, pady=5, padx=5, sticky='w')
+        self.contraseña_entry.grid(row=2, column=1, pady=5, padx=5, sticky='w', columnspan=2)  # Ajusta columnspan para ocupar más columnas
 
-        tk.Button(frame, text="Iniciar Sesión", command=self.iniciar_sesion, bg='lightblue', font=('Arial', 12, 'bold')).grid(row=3, column=0, columnspan=2, pady=10)  
-        tk.Button(frame, text="Registrarse", command=self.mostrar_registro, bg='lightblue', font=('Arial', 12, 'bold')).grid(row=4, column=0, columnspan=2, pady=5)
+        tk.Button(frame, text="Iniciar Sesión", command=self.iniciar_sesion, bg='lightblue', font=('Arial', 12, 'bold')).grid(row=3, column=0, columnspan=3, pady=10)  
+        tk.Button(frame, text="Registrarse", command=self.mostrar_registro, bg='lightblue', font=('Arial', 12, 'bold')).grid(row=4, column=0, columnspan=3, pady=5)
 
         frame.columnconfigure(0, weight=1)
         frame.columnconfigure(1, weight=1)
+        frame.columnconfigure(2, weight=1)
         frame.rowconfigure(0, weight=1)
         frame.rowconfigure(1, weight=1)
         frame.rowconfigure(2, weight=1)
